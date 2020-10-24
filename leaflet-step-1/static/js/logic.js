dabae0fff3db3c9cb52eb20622a7f245d60cb9db
Belinda <bmapesa@yahoo.com>
2020-10-24 00:09:26 -0700

    second commit

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -181,12 +181,13 @@ var baseMaps = {
   // "Dark Map": darkmap
 };
 var earthquakes = new L.LayerGroup()
+
 // Create overlay object to hold our overlay layer
 var overlayMaps = {
   Earthquakes: earthquakes
 };
 
-// Create our map, giving it the streetmap and earthquakes layers to display on load
+// Create our map object, giving it the lightmap and earthquakes layers to display on load
 var myMap = L.map("mapid", {
 center: [
   37.09, -95.71
@@ -195,43 +196,49 @@ zoom: 5,
 layers: [lightmap,  earthquakes]
 });
 
+// add lightmap to myMap
 lightmap.addTo(myMap)
 
+// Store our API endpoint
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
+
+  // create function for earthquake color(depth) and circle sizes (magnitude)
 function markerColor (depth){
   var color = ""
   if (depth > 90){
-    color = "purple"
+    color = "red"
   }
 else if (depth > 70){
-  color = "blue"
+  color = "dark orange"
 }
 else if (depth > 50){
-  color = "green"
+  color = "orange"
 }
 else if (depth > 30){
   color = "yellow"
 }
 else if (depth > 10){
-  color = "orange"
+  color = "green"
 }
-else {color = "red"}
+else {color = "lime"}
 return color
 }
 
+// create function for circle sizes 
 function markerRadius (magnitude){
-  return magnitude * 4
+  return magnitude * 5
 }
+
 
 L.geoJson(data,{
   pointToLayer : function (feature,coordinate){
     return L.circleMarker(coordinate)
   },
 
+// Create pop up with earthquake information 
   onEachFeature: function(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
@@ -242,18 +249,44 @@ style:function(feature){
   return {
     fillColor: markerColor(feature.geometry.coordinates[2]),
     radius: markerRadius(feature.properties.mag),
-    color:"black"
+    color:"black",
+    weight: 1,
+    opacity:1,
+    fillOpacity:0.8
   }
 }
 }).addTo(earthquakes)
 
-earthquakes.addTo(myMap)
+// earthquakes.addTo(myMap)
 });
 
+//  an object legend
+var legend = L.control({
+  position:"bottomright"
+});
 
+// details for the legen
+legend.onAdd =  function(map){
+  var div = L.DomUtil.create("div", "info legend");
 
+  var grades = [-10,10,30,50,70,90];
+ var colors = [
+   "lime",
+  "green",
+  "yellow",
+  "orange",
+  "dark orange",
+  "red"
+  ];
 
+  // looping through the legend label
+  for (var i = 0; i < grades.length; i++) {
+    div.innerHTML +=
+      "<i style='background: " + colors[i] + "'></i> " +
+      grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+  }
+  return div;
+};
 
-
-
-
+//  legend to the map
+legend.addTo(myMap);
---
+++ leaflet-step-2/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiYmVsem1hcHoiLCJhIjoiY2tmb2tseGFxMjNtNzMxcG1lc2VmZnNhMSJ9.Us3b_kThcNOMiogEPzfUiw";
